3c1d4bf679d643059667002dbc87ab05556a690d
Jack Atherton <jackrrye@gmail.com>
2019-07-02 16:11:37 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,6 +17,12 @@ class BankAccount
     "Your balance is $#{balance}."
   end
 
-
+  def valid? 
+    if status == open && balance > 0
+      true 
+    else 
+      false 
+    end 
+  end 
 
 end
