fa7c4b173773086449986b9d686dac11bd457df9
Jack Atherton <jackrrye@gmail.com>
2019-07-02 16:20:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,27 @@
 class Transfer
-  # your code here
+
+  attr_reader :sender, :reciever, :amount
+  attr_accessor :status
+  
+  def initialize(sender, reciever, amount)
+    @sender = sender 
+    @reciever = reciever
+    @amount = amount 
+    @status = "pending"
+  end 
+
+  def valid? 
+    if @sender.valid? && @reciever.valid?
+      true 
+    else 
+      false 
+    end 
+  end 
+
+  
+    
+
+
+
+  
 end
