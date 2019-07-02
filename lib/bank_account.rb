a6f44ab9947b65d3b9839e9f6081f9263b0eb756
Jack Atherton <jackrrye@gmail.com>
2019-07-02 16:05:36 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,17 @@
 class BankAccount
 
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end 
+
+  def deposit(amount) { @balance += amount }
+    
+
+  
+  
 end
